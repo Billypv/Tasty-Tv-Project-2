0b42fcac05ad2761249cbf0a9ecb1efadd746c91
Billypv <billypv14@gmail.com>
2021-12-03 22:48:08 +0000

    removed console.log

--- src/Recomendations.js
+++ src/Recomendations.js
@@ -6,7 +6,6 @@ function Recomendations(props){
     const [filmRecomendations, setFilmRecomendations] = useState({results:[]})
     useEffect(()=>{
         async function getRecomendations(data){
-            console.log(data)
             const chosenGenre = data.genres[Math.floor(Math.random()*data.genres.length)].id
             const genreResponse = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${chosenGenre}&with_watch_monetization_types=flatrate`)
             const genreData = await genreResponse.json()
@@ -16,10 +15,6 @@ function Recomendations(props){
         async function genreFromAPI(){
             const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=`)
             const data = await response.json()
-            // console.log(data)
-            // console.log(filmGenre)
-            // setFilmGenre(data)
-            // console.log(filmGenre)
             getRecomendations(data)
             
         } 
